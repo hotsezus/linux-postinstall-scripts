d17fc7b03ac143491f44e670634f62daf6bec6aa
Alex Derbenev <alex.derbenew@yandex.ru>
2025-03-29 15:15:06 +0300

    Create snx-rs.sh

---
+++ snx-rs.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+if [ "$(id -u)" != "0" ]; then
+   echo "This script must be run as root" 1>&2
+   exit 1
+fi
+
+YELLOW='\033[1;33m'
+NC='\033[0m'
+
+echo -e "${YELLOW}Copy binaries to /opt/snx-rs/${NC}"
+mkdir -vp /opt/snx-rs
+cp --verbose snxctl /opt/snx-rs/snxctl
+cp --verbose snx-rs /opt/snx-rs/snx-rs
+cp --verbose snx-rs-gui /opt/snx-rs/snx-rs-gui
+
+echo -e "${YELLOW}Add desktop entry /usr/share/applications/snx-rs-gui.desktop${NC}"
+cp --verbose snx-rs-gui.desktop /usr/share/applications/snx-rs-gui.desktop
+sed -i 's/^Icon=network-vpn/Icon=network-vpn-symbolic/' /usr/share/applications/snx-rs-gui.desktop
+sed -i 's/^Name=SNX-RS VPN client/Name=SNX/' /usr/share/applications/snx-rs-gui.desktop
+
+echo -e "${YELLOW}Add service /usr/lib/systemd/system/snx-rs.service${NC}"
+cp --verbose snx-rs.service /usr/lib/systemd/system/snx-rs.service
+
+echo -e "${YELLOW}Apply permissions${NC}"
+chmod --verbose 755 /opt/snx-rs/snxctl  /opt/snx-rs/snx-rs /opt/snx-rs/snx-rs-gui
+chmod --verbose 644 /usr/share/applications/snx-rs-gui.desktop
+chmod --verbose 644 /usr/lib/systemd/system/snx-rs.service
+
+echo -e "${YELLOW}Enable service${NC}"
+systemctl daemon-reload
+systemctl enable snx-rs.service
+systemctl start snx-rs.service
+systemctl status snx-rs.service
